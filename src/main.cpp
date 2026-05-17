cee0912ae956616a98f3373e3a0cdcd080db1c79
karimzabirov3537-crypto <karimzabirov3537@gmail.com>
2026-05-17 22:50:28 +0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,38 +6,7 @@ using namespace geode::prelude;
 bool g_isNoclipActive = false;
 bool g_isWaitingForDecision = false;
 
-class DeathDecisionDelegate : public FLAlertLayerProtocol {
-public:
-    void FLAlert_Clicked(FLAlertLayer* layer, bool btn2) override {
-        if (btn2) {
-            g_isNoclipActive = true;
-            Notification::create("Saved! Noclip active!", NotificationIcon::Success)->show();
-
-            // Используем родной планировщик Cocos2d вместо std::thread — 
-            // это гарантирует, что fmt и ninja вообще не полезут в код таймера
-            if (auto pl = PlayLayer::get()) {
-                auto scheduler = cocos2d::CCDirector::sharedDirector()->getScheduler();
-                scheduler->scheduleSelector(
-                    schedule_selector(MyPlayLayer::disableNoclip), 
-                    pl, 
-                    0.0f, 0, 5.0f, false
-                );
-            }
-        } else {
-            g_isNoclipActive = false;
-            g_isWaitingForDecision = false;
-            if (auto pl = PlayLayer::get()) {
-                pl->destroyPlayer(pl->m_player1, nullptr);
-            }
-        }
-        g_isWaitingForDecision = false;
-    }
-};
-
-static DeathDecisionDelegate g_deathDelegate;
-
 class $modify(MyPlayLayer, PlayLayer) {
-    // Выносим функцию отключения в кастомные методы PlayLayer
     void disableNoclip(float dt) {
         g_isNoclipActive = false;
         Notification::create("Noclip deactivated!", NotificationIcon::Warning)->show();
@@ -48,27 +17,29 @@ class $modify(MyPlayLayer, PlayLayer) {
         if (g_isWaitingForDecision) return;
         g_isWaitingForDecision = true;
 
-        // Самый мощный обход fmt: мы создаем обычные переменные типа const char*
-        // и передаем их. Компилятор видит в них чистые указатели памяти, 
-        // а не строковые литералы, поэтому библиотека fmt автоматически СКИПАЕТ их проверку!
-        const char* title = "Are you sure?";
-        const char* desc = "Do you really want to die?";
-        const char* btn1 = "No";
-        const char* btn2 = "Yes";
+        // Самый стабильный метод Geode v3. Никаких fmt, никаких капризных макросов!
+        geode::createQuickPopup(
+            "Are you sure?",            // Заголовок
+            "Do you really want to die?", // Текст
+            "No", "Yes",                // Кнопки
+            [this](auto, bool btn2) {
+                if (btn2) {
+                    g_isNoclipActive = true;
+                    Notification::create("Saved! Noclip active!", NotificationIcon::Success)->show();
 
-        auto alert = FLAlertLayer::create(
-            &g_deathDelegate, 
-            title, 
-            desc, 
-            btn1, 
-            btn2, 
-            300.f
+                    auto scheduler = cocos2d::CCDirector::sharedDirector()->getScheduler();
+                    scheduler->scheduleSelector(
+                        schedule_selector(MyPlayLayer::disableNoclip), 
+                        this, 
+                        0.0f, 0, 5.0f, false
+                    );
+                } else {
+                    g_isNoclipActive = false;
+                    g_isWaitingForDecision = false;
+                    this->destroyPlayer(this->m_player1, nullptr);
+                }
+                g_isWaitingForDecision = false;
+            }
         );
-        
-        if (alert) {
-            alert->show();
-        } else {
-            g_isWaitingForDecision = false;
-        }
     }
 };
