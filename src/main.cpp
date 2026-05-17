6befcb480be2e5092d340a8d7ce52dffab6110da
karimzabirov3537-crypto <karimzabirov3537@gmail.com>
2026-05-17 22:55:38 +0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,34 +12,71 @@ class $modify(MyPlayLayer, PlayLayer) {
         Notification::create("Noclip deactivated!", NotificationIcon::Warning)->show();
     }
 
+    // Обработчик кнопки "Yes"
+    void onYesClicked(cocos2d::CCObject*) {
+        g_isNoclipActive = true;
+        Notification::create("Saved! Noclip active!", NotificationIcon::Success)->show();
+
+        // Удаляем наше кастомное окно с экрана
+        if (auto layer = cocos2d::CCDirector::sharedDirector()->getRunningScene()->getChildByTag(9999)) {
+            layer->removeFromParentAndCleanup(true);
+        }
+
+        // Запускаем таймер на 5 секунд
+        auto scheduler = cocos2d::CCDirector::sharedDirector()->getScheduler();
+        scheduler->scheduleSelector(
+            schedule_selector(MyPlayLayer::disableNoclip), 
+            this, 
+            0.0f, 0, 5.0f, false
+        );
+
+        g_isWaitingForDecision = false;
+    }
+
+    // Обработчик кнопки "No"
+    void onNoClicked(cocos2d::CCObject*) {
+        if (auto layer = cocos2d::CCDirector::sharedDirector()->getRunningScene()->getChildByTag(9999)) {
+            layer->removeFromParentAndCleanup(true);
+        }
+        g_isNoclipActive = false;
+        g_isWaitingForDecision = false;
+        
+        // Принудительно убиваем игрока
+        this->destroyPlayer(this->m_player1, nullptr);
+    }
+
     void destroyPlayer(PlayerObject* p0, GameObject* p1) {
         if (g_isNoclipActive) return;
         if (g_isWaitingForDecision) return;
         g_isWaitingForDecision = true;
 
-        // Самый стабильный метод Geode v3. Никаких fmt, никаких капризных макросов!
-        geode::createQuickPopup(
-            "Are you sure?",            // Заголовок
-            "Do you really want to die?", // Текст
-            "No", "Yes",                // Кнопки
-            [this](auto, bool btn2) {
-                if (btn2) {
-                    g_isNoclipActive = true;
-                    Notification::create("Saved! Noclip active!", NotificationIcon::Success)->show();
+        auto scene = cocos2d::CCDirector::sharedDirector()->getRunningScene();
+        if (!scene) return;
 
-                    auto scheduler = cocos2d::CCDirector::sharedDirector()->getScheduler();
-                    scheduler->scheduleSelector(
-                        schedule_selector(MyPlayLayer::disableNoclip), 
-                        this, 
-                        0.0f, 0, 5.0f, false
-                    );
-                } else {
-                    g_isNoclipActive = false;
-                    g_isWaitingForDecision = false;
-                    this->destroyPlayer(this->m_player1, nullptr);
-                }
-                g_isWaitingForDecision = false;
-            }
-        );
+        // Создаем чистое полупрозрачное Cocos2d-окно поверх игры (0% зависимости от Geode/fmt!)
+        auto customAlert = cocos2d::CCLayerColor::create(cocos2d::ccc4(0, 0, 0, 180));
+        customAlert->setTag(9999);
+
+        // Добавляем текст вопроса
+        auto label = cocos2d::CCLabelBMFont::create("Are you sure you want to die?", "goldFont.fnt");
+        label->setPosition(cocos2d::CCDirector::sharedDirector()->getWinSize() / 2 + cocos2d::CCSize(0, 40));
+        customAlert->addChild(label);
+
+        // Создаем кнопку "Yes"
+        auto yesLabel = cocos2d::CCLabelBMFont::create("Yes", "bigFont.fnt");
+        auto yesBtn = cocos2d::CCMenuItemLabel::create(yesLabel, customAlert, cocos2d::SEL_MenuHandler(&MyPlayLayer::onYesClicked));
+        yesBtn->setPosition(cocos2d::CCPoint(-60, -30));
+
+        // Создаем кнопку "No"
+        auto noLabel = cocos2d::CCLabelBMFont::create("No", "bigFont.fnt");
+        auto noBtn = cocos2d::CCMenuItemLabel::create(noLabel, customAlert, cocos2d::SEL_MenuHandler(&MyPlayLayer::onNoClicked));
+        noBtn->setPosition(cocos2d::CCPoint(60, -30));
+
+        // Собираем меню кнопок
+        auto menu = cocos2d::CCMenu::create(yesBtn, noBtn, nullptr);
+        customAlert->addChild(menu);
+
+        // Показываем на экране
+        scene->addChild(customAlert, 100);
     }
 };
