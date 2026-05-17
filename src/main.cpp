076d66cb44c37e16d717ac61aec9bf91cce54bcd
karimzabirov3537-crypto <karimzabirov3537@gmail.com>
2026-05-17 20:58:12 +0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,30 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
+#include <chrono>
+#include <thread>
 
 using namespace geode::prelude;
 
 bool g_isNoclipActive = false;
-bool g_alreadyAsking = false;
-
-class DeathQuestionDelegate : public FLAlertLayerProtocol {
-    PlayLayer* m_layer;
-public:
-    DeathQuestionDelegate(PlayLayer* layer) : m_layer(layer) {}
-
-    void FLAlert_Clicked(FLAlertLayer* alert, bool btn2) override {
-        g_alreadyAsking = false;
-        
-        if (btn2) { // "No" button pressed
-            g_isNoclipActive = true;
-            m_layer->resume();
-
-            // 5 seconds timer to disable noclip
-            geode::Loader::get()->queueInMainThread([this]() {
-                auto scheduler = CCScheduler::sharedScheduler();
-                scheduler->scheduleSelector(
-                    schedule_selector(DeathQuestionDelegate::disableNoclip),
-                    this, 5.0f, 0, 0.0f, false
-                );
-            });
-            
-            Notification::create("Noclip active for 5 seconds!", NotificationIcon::Success)->show();
-        } else { // "Yes" button pressed
-            g_isNoclipActive = false;
-            m_layer->resetLevel(); 
-        }
-        delete this; // Memory cleanup
-    }
-
-    void disableNoclip(float dt) {
-        g_isNoclipActive = false;
-        Notification::create("Noclip deactivated!", NotificationIcon::Warning)->show();
-        CCScheduler::sharedScheduler()->unscheduleSelector(schedule_selector(DeathQuestionDelegate::disableNoclip), this);
-    }
-};
 
 class $modify(MyPlayLayer, PlayLayer) {
     void destroyPlayer(PlayerObject* player, GameObject* obj) {
+        // Если ноуклип активен — игнорируем смерть
         if (g_isNoclipActive) return; 
-        if (g_alreadyAsking) return; 
-        g_alreadyAsking = true;
 
-        this->pauseGame(true);
+        // Включаем ноуклип спасения
+        g_isNoclipActive = true;
+        
+        // Показываем уведомление на экране
+        Notification::create("Saved! Noclip active for 5 seconds!", NotificationIcon::Success)->show();
 
-        auto delegate = new DeathQuestionDelegate(this);
-        auto alert = FLAlertLayer::create(
-            delegate,
-            "Hold on...", 
-            "Are you sure you want to die?", 
-            "Yes", "No", 
-            300.0f
-        );
-        alert->show();
+        // Запускаем независимый поток-таймер на 5 секунд для отключения
+        std::thread([]() {
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+            geode::Loader::get()->queueInMainThread([]() {
+                g_isNoclipActive = false;
+                Notification::create("Noclip deactivated!", NotificationIcon::Warning)->show();
+            });
+        }).detach();
     }
 };
